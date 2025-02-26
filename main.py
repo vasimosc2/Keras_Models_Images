d0cc07e65b63aac4f8142aac168ca5a5fb5a4343
vasimosc <moshou.basilis1998@gmail.com>
2025-02-26 23:01:32 +0100

    newas

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Disable GPU
 # Load the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
+y_train = tf.keras.utils.to_categorical(y_train, 100)
+y_test = tf.keras.utils.to_categorical(y_test, 100)
 
 # Ensure directories exist
 os.makedirs('saved_models', exist_ok=True)
