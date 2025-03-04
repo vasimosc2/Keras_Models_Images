b1476c082403cd169fa5af5819800a25b512cd13
vasimosc <moshou.basilis1998@gmail.com>
2025-03-04 21:56:40 +0100

    more typos3

--- Counting/peak_ram.py
+++ Counting/peak_ram.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf # type: ignore
 from typing import Tuple
 
-def estimate_max_memory_usage(model: tf.keras.Model, dtype_size: int = 4) -> Tuple[float, float, float]:
+def estimate_max_memory_usage(model: tf.keras.Model, data_dtype_multiplier: int = 4) -> Tuple[float, float, float]:
     """
     ROM (Read-Only Memory) → Memory used to store layer parameters (weights & biases).
     RAM (Random-Access Memory) → Memory used to store activations (input & output tensors).
@@ -13,19 +13,19 @@ def estimate_max_memory_usage(model: tf.keras.Model, dtype_size: int = 4) -> Tup
     for layer in model.layers:
         
         layer_params: int = layer.count_params() #  Number of parameters in the layer (weights & biases).
-        layer_param_memory: int = layer_params * dtype_size #  Converts the number of parameters into bytes.
+        layer_param_memory: int = layer_params * data_dtype_multiplier#  Converts the number of parameters into bytes.
         total_param_memory += layer_param_memory # Adds up all the layer_param_memory of each layer
 
         # Compute activation memory (RAM)
         if isinstance(layer.output, list):
-            output_memory: int = sum(np.prod(out.shape[1:]) * dtype_size for out in layer.output) # I wont be inside there are layer.output is  <class 'keras.src.backend.common.keras_tensor.KerasTensor'>
+            output_memory: int = sum(np.prod(out.shape[1:]) * data_dtype_multiplier for out in layer.output) # I wont be inside there are layer.output is  <class 'keras.src.backend.common.keras_tensor.KerasTensor'>
         else:
-            output_memory: int = np.prod(layer.output.shape[1:]) * dtype_size # If the output shape is 30 x 30 x 32 , the output memmory is  28800 * data_size
+            output_memory: int = np.prod(layer.output.shape[1:]) * data_dtype_multiplier # If the output shape is 30 x 30 x 32 , the output memmory is  28800 * data_size
 
         if isinstance(layer.input, list):
-            input_memory: int = sum(np.prod(inp.shape[1:]) * dtype_size for inp in layer.input)
+            input_memory: int = sum(np.prod(inp.shape[1:]) * data_dtype_multiplier for inp in layer.input)
         else:
-            input_memory: int = np.prod(layer.input.shape[1:]) * dtype_size
+            input_memory: int = np.prod(layer.input.shape[1:]) * data_dtype_multiplier
 
         # Track peak RAM usage
         layer_ram_usage: int = input_memory + output_memory
