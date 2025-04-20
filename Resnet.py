d87b5b1595654a8539a67eb0d6036abaa77f3b55
vasimosc <moshou.basilis1998@gmail.com>
2025-04-20 12:32:24 +0200

    efficient2

--- Resnet.py
+++ Resnet.py
@@ -23,12 +23,21 @@ else:
 
 # Load CIFAR-100 data
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data(label_mode='fine')
-x_train = tf.image.resize(x_train / 255.0, [224, 224])
-x_test = tf.image.resize(x_test / 255.0, [224, 224])
+x_train = x_train / 255.0
+x_test = x_test / 255.0
 
 # One-hot encoding
 y_train = tf.keras.utils.to_categorical(y_train, 100)
 y_test = tf.keras.utils.to_categorical(y_test, 100)
+
+# Use tf.data pipeline to resize efficiently
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)) \
+    .map(lambda x, y: (tf.image.resize(x, [224, 224]), y), num_parallel_calls=tf.data.AUTOTUNE) \
+    .batch(64).prefetch(tf.data.AUTOTUNE)
+
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)) \
+    .map(lambda x, y: (tf.image.resize(x, [224, 224]), y), num_parallel_calls=tf.data.AUTOTUNE) \
+    .batch(64).prefetch(tf.data.AUTOTUNE)
 
 input_shape = (224, 224, 3)
 num_classes = 100
@@ -58,12 +67,11 @@ callbacks = [
 ]
 
 eff_model.fit(
-    x_train, y_train,
-    validation_data=(x_test, y_test),
+    train_ds,
+    validation_data=test_ds,
     epochs=50,
-    batch_size=batch_size,
     callbacks=callbacks
 )
 
-eff_eval = eff_model.evaluate(x_test, y_test, verbose=2)
+eff_eval = eff_model.evaluate(test_ds, verbose=2)
 print(f"✅ EfficientNetB0 Test Accuracy: {eff_eval[1] * 100:.2f}%")
