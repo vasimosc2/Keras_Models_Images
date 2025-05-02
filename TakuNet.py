5c6979ba02b0953e391c59d1c2e458a1ba4b1f9f
vasimosc <moshou.basilis1998@gmail.com>
2025-05-02 12:43:05 +0200

    attempt

--- TakuNet.py
+++ TakuNet.py
@@ -659,12 +659,12 @@ class AdaptiveDropout(tf.keras.layers.Layer):
         super().__init__(**kwargs)
         self.initial_rate = initial_rate
         self.rate = tf.Variable(initial_value=initial_rate, trainable=False, dtype=tf.float32)
-        self.spatial_dropout = tf.keras.layers.SpatialDropout2D(rate=self.initial_rate)
 
     def call(self, inputs, training=False):
         if training:
-            self.spatial_dropout.rate.assign(self.rate)  # Update dynamically
-            return self.spatial_dropout(inputs, training=True)
+            # Dynamically create a new SpatialDropout2D with the current rate
+            dropout_layer = tf.keras.layers.SpatialDropout2D(rate=float(self.rate))
+            return dropout_layer(inputs, training=True)
         else:
             return inputs
 
