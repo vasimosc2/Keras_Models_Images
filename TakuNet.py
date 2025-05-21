5e637d1543854524059c28357b4deccb8503a8f1
vasimosc <moshou.basilis1998@gmail.com>
2025-05-21 23:16:57 +0200

    Change LR

--- TakuNet.py
+++ TakuNet.py
@@ -542,8 +542,8 @@ class TakuNetModel:
             total_epochs:int = self.epochs if self.epochs else self.train_params["num_epochs"]
             loss = tf.keras.losses.CategoricalCrossentropy(label_smoothing=self.train_params["label_smothing"])
             batchSize:int = max(8, int(self.train_params["batch_size"] / 2))
-            initial_lr:float = float(self.train_params["learning_rate"]) if self.learningRate is None else self.learningRate
-
+            #initial_lr:float = float(self.train_params["learning_rate"]) if self.learningRate is None else self.learningRate
+            initial_lr:float = 0.05 if self.model_params["optimizer"].lower() == "sgd" else 0.02
             print(f"The initial Learning rate is {initial_lr}\n")
 
             steps_per_epoch = len(x_train) // batchSize
@@ -560,7 +560,7 @@ class TakuNetModel:
 
 
             optimizer = get_optimizer(name=self.model_params["optimizer"], 
-                                      learning_rate=self.train_params["learning_rate"] if self.learningRate is None else self.learningRate)
+                                      learning_rate=initial_lr)
             
             print(f"The selected optimaizer if {optimizer}\n")
 
